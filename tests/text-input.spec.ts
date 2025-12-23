969cb7783e37fbb7ae5eadb300d63950539807da
jazcala <jazcalabrese@gmail.com>
2025-12-23 15:52:02 -0300

    feat(text input): add test and page object for text input

---
+++ tests/text-input.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '../fixtures/base-test';
+
+test.describe('Text Input Page Specs', () => {
+
+  test.beforeEach(async ({ textInputPage }) => {
+    await textInputPage.navigateTo();
+  });
+
+  test('Verify page title is displayed', async ({ textInputPage }) => {
+    await expect(textInputPage.title).toBeVisible();
+  });
+
+  test('verify button name is changed', async ({ textInputPage }) => {
+
+    let newName: string = "This is my new name";
+    await expect(textInputPage.input).toBeVisible();
+    await textInputPage.setButtonNewName(newName);
+    await expect(textInputPage.button).toHaveText(newName);
+
+  });
+
+});
