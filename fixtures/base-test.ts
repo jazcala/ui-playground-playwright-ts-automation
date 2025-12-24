e0a8c045413ef8cf8d200daea191c019e25bae40
jazcala <jazcalabrese@gmail.com>
2025-12-24 12:53:07 -0300

    feat(verify text): add test and page object for verify text

--- fixtures/base-test.ts
+++ fixtures/base-test.ts
@@ -10,6 +10,19 @@ import { ClickPage } from '../pages/click.page';
 import { TextInputPage } from '../pages/text-input.page';
 import { ScrollbarsPage } from '../pages/scrollbars.page';
 import { DynamicTablePage } from '../pages/dynamic-table.page';
+import { VisibilityPage } from '../pages/visibility.page';
+import { VerifyTextPage } from '../pages/verify-text.page';
+import { ProgressBarPage } from '../pages/progress-bar.page';
+import { SampleAppPage } from '../pages/sample-app.page';
+import { MouseOverPage } from '../pages/mouse-over.page';
+import { NonBreakingSpacePage } from '../pages/non-braking-space.page';
+import { OverlappedElementPage } from '../pages/overlapped-element.page';
+import { ShadowDomPage } from '../pages/shadow-dom.page';
+import { AlertsPage } from '../pages/alerts.page';
+import { FileUploadPage } from '../pages/file-upload.page';
+import { AnimatedButtonPage } from '../pages/animated-button.page';
+import { DisabledInputPage } from '../pages/disabledd-input.page';
+import { AutoWaitPage } from '../pages/auto-wait.page';
 
 type MyPageObjects = {
   landingPage: LandingPage;
@@ -23,6 +36,19 @@ type MyPageObjects = {
   textInputPage: TextInputPage;
   scrollbarsPage: ScrollbarsPage;
   dynamicTablePage: DynamicTablePage;
+  verifyTextPage: VerifyTextPage;
+  progressBarPage: ProgressBarPage;
+  visibilityPage: VisibilityPage;
+  sampleAppPage: SampleAppPage;
+  mouseOverPage: MouseOverPage;
+  nonBreakingSpacePage: NonBreakingSpacePage;
+  overlappedElementPage: OverlappedElementPage;
+  shadowDomPage: ShadowDomPage;
+  alertsPage: AlertsPage;
+  fileUploadPage: FileUploadPage;
+  animatedButtonPage: AnimatedButtonPage;
+  disabledInputPage: DisabledInputPage;
+  autoWaitPage: AutoWaitPage;
 }
 
 export const test = base.extend<MyPageObjects>({
@@ -70,7 +96,59 @@ export const test = base.extend<MyPageObjects>({
   dynamicTablePage: async ({ page }, use) => {
     const dynamicTablePage = new DynamicTablePage(page);
     await use(dynamicTablePage);
-  }
+  },
+  verifyTextPage: async ({ page }, use) => {
+    const verifyTextPage = new VerifyTextPage(page);
+    await use(verifyTextPage);
+  },
+  progressBarPage: async ({ page }, use) => {
+    const progressBarPage = new ProgressBarPage(page);
+    await use(progressBarPage);
+  },
+  visibilityPage: async ({ page }, use) => {
+    const visibilityPage = new VisibilityPage(page);
+    await use(visibilityPage);
+  },
+  sampleAppPage: async ({ page }, use) => {
+    const sampleAppPage = new SampleAppPage(page);
+    await use(sampleAppPage);
+  },
+  mouseOverPage: async ({ page }, use) => {
+    const mouseOverPage = new MouseOverPage(page);
+    await use(mouseOverPage);
+  },
+  nonBreakingSpacePage: async ({ page }, use) => {
+    const nonBreakingSpacePage = new NonBreakingSpacePage(page);
+    await use(nonBreakingSpacePage);
+  },
+  overlappedElementPage: async ({ page }, use) => {
+    const overlappedElementPage = new OverlappedElementPage(page);
+    await use(overlappedElementPage);
+  },
+  shadowDomPage: async ({ page }, use) => {
+    const shadowDomPage = new ShadowDomPage(page);
+    await use(shadowDomPage);
+  },
+  alertsPage: async ({ page }, use) => {
+    const alertsPage = new AlertsPage(page);
+    await use(alertsPage);
+  },
+  fileUploadPage: async ({ page }, use) => {
+    const fileUploadPage = new FileUploadPage(page);
+    await use(fileUploadPage);
+  },
+  animatedButtonPage: async ({ page }, use) => {
+    const animatedButtonPage = new AnimatedButtonPage(page);
+    await use(animatedButtonPage);
+  },
+  disabledInputPage: async ({ page }, use) => {
+    const disabledInputPage = new DisabledInputPage(page);
+    await use(disabledInputPage);
+  },
+  autoWaitPage: async ({ page }, use) => {
+    const autoWaitPage = new AutoWaitPage(page);
+    await use(autoWaitPage);
+  },
 });
 
 export { expect } from '@playwright/test';
--- pages/verify-text.page.ts
+++ pages/verify-text.page.ts
@@ -6,10 +6,12 @@ export class VerifyTextPage extends BasePage {
 
   readonly title: Locator;
   protected readonly path = URL_PATH_CONTSTANTS.VERIFY_TEXT;
+  readonly text: Locator;
 
   constructor(page: Page) {
     super(page);
     this.title = page.getByRole('heading', { name: 'Verify Text' });
+    this.text = page.locator('.badge-secondary').filter({ hasText: /Welcome .*!/ });
   }
 
 }
